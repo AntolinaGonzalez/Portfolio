b9f784060b1bb7f4a9c4c52fbf43f492ffa9fe44
Antolina Gonzalez <antolina487@gmail.com>
2020-10-18 16:08:07 -0300

    feature: styles

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,32 @@
-import Head from "next/head";
 import style from "../styles/Home.module.css";
-import {Popover, OverlayTrigger, Button} from 'react-bootstrap'
+import { Popover, OverlayTrigger, Button } from "react-bootstrap";
+
 export default function Home() {
   const popover = (
     <Popover id="popover-basic">
       <Popover.Title as="h3">Case-Sensitive</Popover.Title>
       <Popover.Content>
-      Uppercase and Lowercase letters are treated as distinc
+        Uppercase and Lowercase letters are treated as distinc
       </Popover.Content>
     </Popover>
   );
   const Example = () => (
     <OverlayTrigger trigger="click" placement="right" overlay={popover}>
-      <Button className={style.buttonStyle} variant="warning">?</Button>
+      <Button className={style.buttonStyle} variant="warning">
+        ?
+      </Button>
     </OverlayTrigger>
   );
 
   return (
     <div className={style.backColor}>
       <div className={style.principal}>
-      <h1>Antolina's Portfolio </h1>
-      <h2>FullStack developer</h2>
+        <h1>Antolina's Portfolio </h1>
+        <h2>FullStack developer</h2>
       </div>
       <div className={style.division}>
         <div>
-          <h3>Instructions:</h3>
+          <h3 className={style.centerStyle}>Instructions:</h3>
           <h5>You can manage to terminal to get to know me</h5>
           <h5>if you wanna know what are enter 'ls' in the terminal</h5>
           <h5>cd 'option' to see the context</h5>
@@ -33,18 +35,23 @@ export default function Home() {
           <h5>Try! its fun</h5>
 
           <h4>Warning!</h4>
-          <h5>the commands are case-sensitive <Example /></h5>
-          
+          <h5>
+            the commands are case-sensitive <Example />
+          </h5>
         </div>
-        <div>
-          <h3>Terminal</h3>
+        <div className={style.terminalSpace}>
+          <h3 className={style.centerStyle}>Terminal</h3>
           <div className={style.terminal}>
             <div className={style.terminalHead}>
               <p>antolina@antolina-portfolio</p>
             </div>
             <form className={style.colorLabel} action="api/redirect">
               <label>antolina@antolina-portfolio:~/</label>
-              <input type="text" name="option" className={style.inputStyle}></input>
+              <input
+                type="text"
+                name="option"
+                className={style.inputStyle}
+              ></input>
             </form>
           </div>
         </div>
--- pages/ls.js
+++ pages/ls.js
@@ -1,36 +1,66 @@
-import Head from "next/head";
 import style from "../styles/Home.module.css";
-export default function Home() {
+import { Popover, OverlayTrigger, Button } from "react-bootstrap";
+export default function Ls() {
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Title as="h3">Case-Sensitive</Popover.Title>
+      <Popover.Content>
+        Uppercase and Lowercase letters are treated as distinc
+      </Popover.Content>
+    </Popover>
+  );
+  const Example = () => (
+    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+      <Button className={style.buttonStyle} variant="warning">
+        ?
+      </Button>
+    </OverlayTrigger>
+  );
   return (
     <div className={style.backColor}>
       <div className={style.principal}>
-      <h1>Antolina's Portfolio </h1>
-      <h2>FullStack developer</h2>
+        <h1>Antolina's Portfolio </h1>
+        <h2>FullStack developer</h2>
       </div>
       <div className={style.division}>
         <div>
-          <h1>Instrucctions:</h1>
-          <h4>You can manage to terminal to get to know me</h4>
-          <h4>if you wanna know what are enter 'ls' in the terminal</h4>
+          <h3 className={style.centerStyle}>Instructions:</h3>
+          <h5>You can manage to terminal to get to know me</h5>
+          <h5>if you wanna know what are enter 'ls' in the terminal</h5>
+          <h5>cd 'option' to see the context</h5>
+          <h5>p.e : cd Experience </h5>
+          <h5>and you will be render to my experience page</h5>
+          <h5>Try! its fun</h5>
+
+          <h4>Warning!</h4>
+          <h5>
+            the commands are case-sensitive <Example />
+          </h5>
         </div>
-        <div>
-          <h1>Terminal</h1>
+        <div className={style.terminalSpace}>
+          <h3 className={style.centerStyle}>Terminal</h3>
           <div className={style.terminal}>
             <div className={style.terminalHead}>
               <p>antolina@antolina-portfolio</p>
             </div>
-            <p style={{color:"white"}}>antolina@antolina-portfolio:~$ ls</p>
+            <p style={{ color: "white", margin: 0, marginLeft:'10px' }}>
+              antolina@antolina-portfolio:~$ ls
+            </p>
             <div className={style.outputTerm}>
-            <p>Experience</p>
-            <p>Projects</p>
-            <p>Skilss</p>
-            <p>Education</p>
-            <p>Contact</p>
+              <p>Contact</p>
+              <p>Education</p>
+              <p>Experience</p>
+              <p>Projects</p>
+              <p>Skilss</p>
             </div>
-           
+
             <form className={style.colorLabel} action="api/redirect">
               <label>antolina@antolina-portfolio:~/</label>
-              <input type="text" name="option" className={style.inputStyle}></input>
+              <input
+                type="text"
+                name="option"
+                className={style.inputStyle}
+              ></input>
             </form>
           </div>
         </div>
