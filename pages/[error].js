c07bb7fc6bf68887ea63091d7b540cf63e265aed
Antolina Gonzalez <antolina487@gmail.com>
2020-10-23 10:53:53 -0300

    fix: instructions compnent

--- pages/[error].js
+++ pages/[error].js
@@ -1,5 +1,6 @@
 import style from "../styles/Home.module.css";
-import { Popover, OverlayTrigger, Button } from "react-bootstrap";
+import Instructions from "../components/instructions";
+
 
 Error.getInitialProps = async ({ query }) => {
   const cd=query.cd
@@ -8,22 +9,7 @@ Error.getInitialProps = async ({ query }) => {
   };
 }
 export default function Error(props) {
-  const popover = (
-    <Popover id="popover-basic">
-      <Popover.Title as="h3">Case-Sensitive</Popover.Title>
-      <Popover.Content>
-        Uppercase and Lowercase letters are treated as distinc
-      </Popover.Content>
-    </Popover>
-  );
-  const Example = () => (
-    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
-      <Button className={style.buttonStyle} variant="warning">
-        ?
-      </Button>
-    </OverlayTrigger>
-  );
-
+ 
   return (
     <div className={style.backColor}>
       <div className={style.principal}>
@@ -31,20 +17,7 @@ export default function Error(props) {
         <h2>FullStack developer</h2>
       </div>
       <div className={style.division}>
-        <div>
-          <h3 className={style.centerStyle}>Instructions:</h3>
-          <h5>You can manage to terminal to get to know me</h5>
-          <h5>if you wanna know what are enter 'ls' in the terminal</h5>
-          <h5>cd 'option' to see the context</h5>
-          <h5>p.e : cd Experience </h5>
-          <h5>and you will be render to my experience page</h5>
-          <h5>Try! its fun</h5>
-
-          <h4>Warning!</h4>
-          <h5>
-            the commands are case-sensitive <Example />
-          </h5>
-        </div>
+      <Instructions></Instructions>
         <div className={style.terminalSpace}>
           <h3 className={style.centerStyle}>Terminal</h3>
           <div className={style.terminal}>
